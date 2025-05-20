2ec058fec790e9214b8bcd546145165e5c7f1219
abhijitbhowmick <abhijitbhowmick2021@gmail.com>
2025-05-20 15:30:00 +0200

    1st commit

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+app.use(express.json());
+
+const router = express.Router();
+module.exports = router;
+const rootRouter = require("./routes/root");
+app.use("/user", rootRouter);
+app.use(cors());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+}
+);
 
 
---
+++ backend/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router =express.Router();
+const userrouter = require('./user');
+
+router.use('/user', userrouter);
+module.exports = router;
